b12aab211bb838f21adb3d92ff8590523415da53
nahida1234 <nahida_036@yahoo.com>
2014-10-18 12:53:51 +0600

    ResultSheetUI.cs added

---
+++ StudentCourse/StudentCourse/UI/ResultSheetUI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResultSheetUI
+{
+    public partial class ResultSheetUI : Form
+    {
+        public ResultSheetUI()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
