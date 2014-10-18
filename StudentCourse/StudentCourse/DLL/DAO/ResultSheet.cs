33f777df0820d677717c8b2295acc5f9dadb789a
nahida1234 <nahida_036@yahoo.com>
2014-10-18 12:57:00 +0600

    cs added

---
+++ StudentCourse/StudentCourse/DLL/DAO/ResultSheet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultSheetUI.DLL.DAO
+{
+    class ResultSheet
+    {
+        public string RegNo { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public double Result { set; get; }
+
+        public double Score { set; get; }
+
+        public double AverageScore(double score)
+        {
+            double average=
+        }
+
+        public string GradeLetter(double AverageScore)
+        {
+            if (AverageScore >= 90)
+                return "A";
+            else if (AverageScore >= 70 && AverageScore < 90)
+                return "B";
+            else if (AverageScore >= 50 && AverageScore < 70)
+                return "C";
+            else if (AverageScore < 50)
+                return "F";
+         
+
+        }
+    }
+}
---
+++ StudentCourse/StudentCourse/DLL/GateWay/ResultSheetGateWay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResultSheetUI.DLL.GATEWAY
+{
+    internal class ResultSheetGateWay
+    {
+        public SqlConnection connection;
+
+        public ResultSheetGateWay()
+        {
+            string conn = @"server=BITM-401-PC06\SQLEXPRESS; database=ResultSheet; integrated security=true";
+            connection = new SqlConnection();
+            connection.ConnectionString = conn;
+
+        }
+
+        public string Save(ResultSheet aResultSheet)
+        {
+            connection.open();
+            string query = string.Format("INSERT INTO ResultSheet VALUES ('{0}','{1}','{2}','{3}')", aResultSheet.RegNo,
+                aResultSheet.Name, aResultSheet.Email, aResultSheet.Score);
+            SqlCommand command = new SqlCommand(query, connection);
+
+            int affectedRows = command.ExecuteNonQuery();
+            connection.Close();
+            if (affectedRows > 0)
+                return "insert success";
+            return "something wrong";
+        }
+
+        public bool HasThisRegNoValid(string RegNo)
+        {
+            connection.Open();
+            string query = string.Format("SELECT * FROM ResultSheet WHERE RegNo='{0}'", RegNo);
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader aReader = command.ExecuteReader();
+            bool Hasrow = aReader.HasRows;
+            connection.Close();
+            return Hasrow;
+        }
+    }
+}
